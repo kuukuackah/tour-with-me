b9c3475ffa66f8ef58ee941fffa48b4dbe9813d5
kuukuackah <98889348+kuukuackah@users.noreply.github.com>
2023-03-07 17:26:41 +0000

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -14,11 +14,19 @@ var typed = new Typed("#typed", {
   loopCount: Infinity,
 });
 
-var swiper = new Swiper(".swiper", {
-  slidesPerView: 3,
-
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
 });
