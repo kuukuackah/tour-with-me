ee8eefedd24168c6d30812e8c74a6f491a1cc6d3
kuukuackah <98889348+kuukuackah@users.noreply.github.com>
2023-03-15 01:35:27 +0000

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -30,3 +30,37 @@ var swiper = new Swiper(".mySwiper", {
     el: ".swiper-pagination",
   },
 });
+
+const images = document.querySelectorAll('.gallery img');
+let index = 0;
+
+function changeImage() {
+  images[index].classList.remove('active');
+  index = (index + 1) % images.length;
+  images[index].classList.add('active');
+}
+
+setInterval(changeImage, 3000);
+
+
+const form = document.querySelector('#contact-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const messageInput = document.querySelector('#message');
+const submitBtn = document.querySelector('#submit-btn');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  // Validate the form fields
+  if (nameInput.value === '' || emailInput.value === '' || messageInput.value === '') {
+    alert('Please fill in all fields.');
+    return;
+  }
+
+  // Show an alert message
+  alert('Your message has been sent. Thank you!');
+
+  // Clear the form fields
+  form.reset();
+});
